d576d108920e59220bcf07ebc2dbc61b02f35b4b
99003653 <78849726+99003653@users.noreply.github.com>
2021-02-25 17:28:53 +0530

    Update GourmetOrganicShop.cs

--- ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
@@ -8,6 +8,24 @@ namespace ProblemThree_CompleteSolution_BL
 {
     public class GourmetOrganicShop : GourmetShop,ITaxCompute
     {
-        
+        public double TotalBillValue { get; set; }
+        public Dictionary<int, double> GourmetOrganicshop(int ICode, string IName, Dictionary<int, double> price)
+        {
+            return price;
+        }
+        public double PayPerWeight(int weight)
+        {
+            
+            return 1;
+        }
+        public double PayPerPiece(int quantity)
+        {
+            return 1;
+        }
+        public int TaxOnTotalBill()
+
+        {
+            return 1;
+        }
     }
 }
